925acebc236471c67659dd2511e5c6a7ebf48a4b
Nicholas Chung <92408438+nschung28@users.noreply.github.com>
2023-10-15 10:44:56 -0700

    final commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np;
 import librosa
 import librosa.display
  
-# Initialize the recognizer 
+
 r = sr.Recognizer() 
  
 def SpeakText(command):
@@ -12,18 +12,51 @@ def SpeakText(command):
     engine.say(command) 
     engine.runAndWait()
 
-def is_whistling(sample_rate, audio_data): #tester will build off into sirens iff works
+# def is_(sample_rate, audio_data): #tester will build off into sirens iff works
+#     audio_data = audio_data.astype(np.float32) / 32767.0
 
+#     fft_result = np.fft.fft(audio_data)
+#     frequencies = np.fft.fftfreq(len(fft_result), 1 / sample_rate)
+#     curr_frequency = np.abs(frequencies[np.argmax(np.abs(fft_result))])
+
+#     print(curr_frequency)
+
+#     low_freq = 1000  # Adjust this value based on your use case
+#     high_freq = 3500  # Adjust this value based on your use case
+
+#     if low_freq < curr_frequency < high_freq:
+#         return True
+#     else:
+#         return False
+    
+def is_siren(sample_rate, audio_data): #tester will build off into sirens iff works
     audio_data = audio_data.astype(np.float32) / 32767.0
 
-    mfccs = librosa.feature.mfcc(y = audio_data, sr = sample_rate, n_mfcc = 13)
+    frequencies = np.fft.fftfreq(len(np.fft.fft(audio_data)), 1 / sample_rate)
+    curr_frequency = np.abs(frequencies[np.argmax(np.abs(np.fft.fft(audio_data)))])
 
-    mean_mfccs = np.mean(mfccs, axis=1)
-    print(mean_mfccs[1])
-    low_pitch_threshold = 60 #may need to adjust here and line 20
-    high_pitch_threshold = 15
+    print(curr_frequency)
 
-    if low_pitch_threshold < mean_mfccs[1] < high_pitch_threshold:
+    low_freq = 1000  # Adjust this value based on your use case
+    high_freq = 3000  # Adjust this value based on your use case
+
+    if low_freq < curr_frequency < high_freq:
+        return True
+    else:
+        return False
+    
+def is_honk(sample_rate, audio_data): #tester will build off into sirens iff works
+    audio_data = audio_data.astype(np.float32) / 32767.0
+
+    frequencies = np.fft.fftfreq(len(np.fft.fft(audio_data)), 1 / sample_rate)
+    curr_frequency = np.abs(frequencies[np.argmax(np.abs(np.fft.fft(audio_data)))])
+
+    print(curr_frequency)
+
+    low_freq = 600  # Adjust this value based on your use case
+    high_freq = 900  # Adjust this value based on your use case
+
+    if low_freq < curr_frequency < high_freq:
         return True
     else:
         return False
@@ -35,24 +68,21 @@ while(True):
          
         with sr.Microphone() as source2:
             
-            r.adjust_for_ambient_noise(source2, duration=.75)
+            r.adjust_for_ambient_noise(source2, duration=.4)
             audio = r.listen(source2)
             sample_rate = audio.sample_rate
         
         audio_data = np.frombuffer(audio.frame_data, dtype = np.int16)
-        print(type(audio_data))
 
-        if (is_whistling(sample_rate, audio_data)):
-            print("WHISTLING")
+        if (is_siren(sample_rate, audio_data)):
+            print("SIREN DETECTED")
+        elif (is_honk(sample_rate, audio_data)) :
+            print("HONK DETECTED")
         else:    
-            MyText = r.recognize_google(audio)
-            MyText = MyText.lower()
- 
-            print("Did you say ",MyText)
-            SpeakText(MyText)
+            print("SPEECH DETECTED")
              
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
          
-    except sr.UnknownValueError:
-        print("unknown error occurred")
+    except sr.UnknownValueError: #weird sound
+        print("sound not detected")
